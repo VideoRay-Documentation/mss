3142ec5158617c1a09b226e9c41674a88598448e
Tom Glebas <tom.glebas@videoray.com>
2017-02-09 13:02:36 -0500

    update for transfer to Surface

---
+++ javascript/qs_predive.js
@@ -0,0 +1,37 @@
+if (document.images) {
+   cockpit = new Image;
+   cockpitBase = new Image;
+   video = new Image;
+   instruments = new Image;
+   control = new Image;
+
+   controller = new Image;
+   controllerBase = new Image;
+   joystick = new Image;
+   depth = new Image;
+   bright = new Image;
+   dim = new Image;
+   up = new Image;
+   down = new Image;
+   inx = new Image;
+   out = new Image;
+   reserved_2 = new Image;
+   
+   cockpit.src = '../../pro_4/images/vrcockpit_baseline.png';
+   cockpitBase.src = '../../pro_4/images/vrcockpit_baseline.png';
+   video.src = '../../pro_4/images/vrcockpit_video_highlight.png';
+   instruments.src = '../../pro_4/images/vrcockpit_instruments_highlight.png';
+   control.src = '../../pro_4/images/vrcockpit_control_highlight.png';
+
+   controller.src = '../../pro_4/images/hand_controller_v1rb.png';
+   controllerBase.src = '../../pro_4/images/hand_controller_v1rb.png';
+   joystick.src = '../../pro_4/images/hand_controller_v1rb_joystick.png';
+   depth.src = '../../pro_4/images/hand_controller_v1rb_depth.png';
+   bright.src = '../../pro_4/images/hand_controller_v1rb_bright.png';
+   dim.src = '../../pro_4/images/hand_controller_v1rb_dim.png';
+   up.src = '../../pro_4/images/hand_controller_v1rb_up.png';
+   down.src = '../../pro_4/images/hand_controller_v1rb_down.png';
+   inx.src = '../../pro_4/images/hand_controller_v1rb_inx.png';
+   out.src = '../../pro_4/images/hand_controller_v1rb_out.png';
+   reserved_2.src = '../../pro_4/images/hand_controller_v1rb_reserved_2.png';
+   }
